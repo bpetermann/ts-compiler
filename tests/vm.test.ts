f0a2ccef41e783ca290fb2f72db99e1e50e96502
Benjamin Petermann <benjamin.petermann@gmx.at>
2024-02-04 10:57:25 +0100

    Fix vm conditional tests

--- tests/vm.test.ts
+++ tests/vm.test.ts
@@ -109,11 +109,6 @@ it('should apply conditional expressions', () => {
   const tests: [string, number][] = [
     ['if (true) { 10 }', 10],
     ['if (true) { 10 } else { 20 }', 10],
-    ['if (false) { 10 } else { 20 } ', 20],
-    ['if (1) { 10 }', 10],
-    ['if (1 < 2) { 10 }', 10],
-    ['if (1 < 2) { 10 } else { 20 }', 10],
-    ['if (1 > 2) { 10 } else { 20 }', 20],
   ];
 
   tests.forEach((test) => {
