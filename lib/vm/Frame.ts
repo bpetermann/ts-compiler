663096c16086b4a763a4f17ad2d055dbab46b32b
Benjamin Petermann <benjamin.petermann@gmx.at>
2024-03-06 15:22:38 +0100

    Restructure frame class

--- lib/vm/Frame.ts
+++ lib/vm/Frame.ts
@@ -2,10 +2,9 @@ import { Instruction } from '../code';
 import * as obj from '../object';
 
 export default class Frame {
-  fn: obj.CompiledFunction;
   ip: number;
 
-  constructor(fn: obj.CompiledFunction) {
+  constructor(private readonly fn: obj.CompiledFunction) {
     this.fn = fn;
     this.ip = -1;
   }
